82c3864a8ec5dcb514ff8ee16d8164cf0b272745
Belchior Soares <belchiorsoares99@gmail.com>
2024-11-07 11:52:11 +0100

    Criacao do menu

--- Calculadora.c
+++ Calculadora.c
@@ -8,6 +8,18 @@ int main(void)
     float numero1, numero2 = 0;
     float soma, subtracao, multiplica, divisa, potencia, radiciacao = 0;
     int escolha = 0;
-    
+
+    // Criacao do Menu
+    printf("\tCalculdora inteligente\n");
+    printf("1.Adição\n");
+    printf("2.Subtração\n");
+    printf("3.multilicação\n");
+    printf("4.divisão\n");
+    printf("5.Potenciação\n");
+    printf("6.Radiciação\n");
+    printf("0.Sair\n");
+    printf("Escolha aqui: ");
+    scanf("%d", &escolha);
+
     return (0);
 }
