934fce846be266b404597fa043bddd29f2a71f7b
Belchior Soares <belchiorsoares99@gmail.com>
2024-11-07 11:33:27 +0100

    criacao de variaveis

--- Calculadora.c
+++ Calculadora.c
@@ -4,6 +4,10 @@
 
 int main(void)
 {
+    // Creação de variaveis
+    float numero1, numero2 = 0;
+    float soma, subtracao, multiplica, divisa, potencia, radiciacao = 0;
+    int escolha = 0;
     
     return (0);
 }
