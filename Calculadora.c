dd921d2a0a551b840cfab6e6a80f0b9d49333e98
Belchior Soares <belchiorsoares99@gmail.com>
2024-11-07 12:26:52 +0100

    Tratamento de entrada de dados

--- Calculadora.c
+++ Calculadora.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
+// metodos de tratamento de informação
+void inforError(void);
+
 int main(void)
 {
     // Creação de variaveis
@@ -21,5 +24,48 @@ int main(void)
     printf("Escolha aqui: ");
     scanf("%d", &escolha);
 
+    // Tratamento de entrada de dados
+    if (escolha == 1)
+    {
+        printf("Informe a Primeira parcela: ");
+        scanf("%f", &numero1);
+        printf("Informe a segunda parcela: ");
+        scanf("%f", &numero2);
+    }
+    else if (escolha == 2)
+    {
+        printf("Informe o Minuindo: ");
+        scanf("%f", &numero1);
+        printf("Informe o subtraindo: ");
+        scanf("%f", &numero2);
+    }
+    else if (escolha == 3)
+    {
+        printf("Informe o primeiro factor: ");
+        scanf("%f", &numero1);
+        printf("Informe o segundo factor: ");
+        scanf("%f", &numero2);
+    }
+    else if (escolha == 4)
+    {
+        printf("Informe o dividendo: ");
+        scanf("%f", &numero1);
+        printf("Informe o divisor: ");
+        scanf("%f", &numero2);     
+    }
+    else if (escolha == 5)
+    {
+        printf("Informe a base: ");
+        scanf("%f", &numero1);
+        printf("Informe o expoente: ");
+        scanf("%f", &numero2);
+    }
+    else if (escolha == 6)
+    {
+        printf("Informe o radicando: ");
+        scanf("%f", &numero1);
+    }
+    else
+        inforError();
     return (0);
 }
